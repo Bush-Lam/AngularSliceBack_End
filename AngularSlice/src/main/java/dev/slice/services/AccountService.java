4d0cb5a49c236bddbf8f295a2a5573b9da0c4a3c
Bush Lam <bushlam28@gmail.com>
2020-02-14 09:26:48 -0500

    Revert "Merge branch 'master' of https://github.com/VungDuecTeu/AngularSliceBack_End"
    This reverts commit a1f89137461d86153deaba34d1acbb78d971c6cc, reversing
    changes made to 8a42b1252b6497066ec8b3c1cff6ef552308bed5.

--- AngularSlice/src/main/java/dev/slice/services/AccountService.java
+++ AngularSlice/src/main/java/dev/slice/services/AccountService.java
@@ -1,21 +1,7 @@
 package dev.slice.services;
 
-import java.util.Set;
-
 import dev.slice.entities.Account;
 
 public interface AccountService {
-	
-	public Account createAccount(Account account);
-	
-	public Set<Account> getAllAccounts();
-	
-	public Account getAccountById(int id);
-	
-	public boolean deleteAccount(Account account);
-	
-	public Account updateAccount(Account account);
-
-
-
+	public Account login(Account account);
 }
