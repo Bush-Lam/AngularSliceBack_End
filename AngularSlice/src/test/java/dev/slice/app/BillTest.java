6ab80b3fdc14a0147d1e1dfbdc544f1f8ec63172
inamjr <ikeogu56@gmail.com>
2020-02-14 08:17:49 -0600

    bill

--- AngularSlice/src/test/java/dev/slice/app/BillTest.java
+++ AngularSlice/src/test/java/dev/slice/app/BillTest.java
@@ -2,6 +2,9 @@ package dev.slice.app;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.List;
+import java.util.Optional;
+
 import javax.transaction.Transactional;
 
 import org.junit.jupiter.api.MethodOrderer;
@@ -19,26 +22,60 @@ import dev.slice.repositories.AccountRepo;
 import dev.slice.repositories.BillRepo;
 
 @SpringBootTest
-@Transactional
-@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+//@Transactional
+//@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class BillTest {
 
 	@Autowired
 	BillRepo br;
+	@Autowired
 	AccountRepo ar;
 	
+//	@Test
+//	@Commit
+//	@Order(1)
+//	void addBill() {
+//
+//		Bill b= new Bill();
+//		b.setbId(1);
+//		b.setTotal(20);
+//		b.setOrderDate("2020-02-13 04:07:03");
+//		b.setAccount(ar.findById(1).get());
+//		br.save(b);
+//		System.out.println(b +" Has been Added");
+//	}
+	
+//	@Test
+//	@Commit
+//	@Order(2)
+//	void updateBillTot() {
+//		
+//		Bill bill = br.findById(1).get();
+//		bill.setTotal(60);
+//		br.save(bill);
+//		System.out.println(bill+" (Tot Has been Updated)");
+//	}
+//	
 	@Test
-	@Commit
-	@Order(1)
-	void addBill() {
-
-		Bill b= new Bill();
-		b.setbId(1);
-		b.setTotal(20);
-		b.setOrderDate("2020-02-13 04:07:03");
-		b.setAccount(ar.findById(1).get());
-		
-		
+	void findById() {
+		Bill bil = br.findById(2).get();
+		System.out.println(bil);
 	}
+//	
+//	@Test
+//	@Order(3)
+//	void findByAcctId() {
+//		int aid = 2;
+//		Optional<Bill> bil = br.findById(aid);
+//		System.out.println(bil);
+//	}
+//	
+//	@Test
+//	@Order(4)
+//	void findBytotal() {
+//		
+//		List<Bill> bil = br.findByTotalGreaterThan(10);
+//		System.out.println(bil);
+//	}
 
 }
